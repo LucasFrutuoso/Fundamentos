1814f23cf00654fa0f0517cbebb635a604722627
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 16:24:04 -0300

    Create exercicio01.java
    Exercício de fundamentos em Java (Iniciante).

---
+++ exercicio01.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+    
+    // 01
+		System.out.println("Alô mundo!");
+    }
+    }

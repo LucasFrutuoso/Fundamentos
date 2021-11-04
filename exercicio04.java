181c2c1379bbdeae482cc29bfbe98838cab6a324
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 16:37:29 -0300

    Create exercicio04.java
    exercicio04.java

---
+++ exercicio04.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+
+Scanner num = new Scanner(System.in);
+
+		System.out.println("Nota 1:");
+			double n1 = num.nextDouble();
+    
+		System.out.println("Nota 2:");
+			double n2 = num.nextDouble();
+    
+		System.out.println("Nota 3:");
+			double n3 = num.nextDouble();
+    
+		System.out.println("Nota 4:");
+			double n4 = num.nextDouble();
+
+		 double media = (n1 + n2 + n3 + n4) / 4;
+
+		System.out.printf("%.1f", media);
+
+}
+}

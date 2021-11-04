ac7b61398d55ef4f2ddf6529aefc77917a1b5564
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 16:34:53 -0300

    Create exercicio03.java
    exercicio03

---
+++ exercicio03.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+  
+		Scanner num = new Scanner(System.in);
+		
+		System.out.println("Valor 1: ");
+			int n1 = num.nextInt();
+		System.out.println("Valor 2: ");
+			int n2 = num.nextInt();
+			
+		System.out.printf("Soma: %d ", n1 + n2);
+    }
+    }

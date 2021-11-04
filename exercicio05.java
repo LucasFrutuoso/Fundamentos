d5291610478d67ac591a29d703734554cb9c2c78
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 16:41:17 -0300

    Create exercicio05.java
    exercicio05

---
+++ exercicio05.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+		
+		Scanner num = new Scanner(System.in);
+		
+		System.out.println("Quantos metros? ");
+		
+    double Metros = num.nextDouble();
+    double conversao = Metros * 100; //Conversão M para cm
+    
+		System.out.printf("%.0f cm.", conversao);
+		
+		}
+    }

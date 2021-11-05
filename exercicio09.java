52385ac30609c656066c8e6c1565ef3ffa8c1b97
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 23:49:33 -0300

    Create exercicio09.java

---
+++ exercicio09.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+		
+
+
+
+// Fórmula conversão temp. --> (°F -32) x 5/9 = C°
+
+				final int ajuste = 32;
+				final int n1 = 5;
+				final int n2 = 9;
+				
+				Scanner num = new Scanner(System.in);
+				
+				System.out.println("Fahrenheit: ");
+				 double F = num.nextDouble();
+				
+				
+				System.out.printf("%.2f C°", (F - ajuste) * n1/n2);
+        }
+        }

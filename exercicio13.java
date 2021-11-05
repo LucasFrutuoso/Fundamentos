06a41222e7ee471e247b69c117c0b66a0be6be7b
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 23:59:41 -0300

    Create exercicio13.java

---
+++ exercicio13.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+		
+		Scanner num = new Scanner(System.in);	
+		
+		double h = 1.89;
+		double pesoHomen = (72.7 * h) - 58;
+		double pesoMulher = (62.1 * h) - 44.7;
+		
+		System.out.printf("Peso ideal é: %.2f Kg para homens e %.2f Kg para mulheres.", pesoHomen, pesoMulher);
+
+		}
+    }

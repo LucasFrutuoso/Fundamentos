a6e4698f96e90c0d5294b3d017aa82d9f0193ddb
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 23:58:14 -0300

    Create exercicio12.java

---
+++ exercicio12.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+		
+
+		Scanner num = new Scanner(System.in);	
+		
+		
+		double A = 1.87;
+		double pesoIdeal = (72.7 * A) - 58;
+		
+		System.out.printf("O peso ideal é: %.2f Kg.", pesoIdeal);
+  }
+}

1a24ece9ddd7110d33eb5a0c5ea5289a6a9407b3
LucasFrutuoso <93358834+LucasFrutuoso@users.noreply.github.com>
2021-11-04 23:46:53 -0300

    Create exercicio08.java

---
+++ exercicio08.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicios {
+
+	public static void main(String[] args) {
+		
+
+		
+
+		Scanner num = new Scanner(System.in);
+		
+		System.out.println("Quantos reais você recebe por hora trabalhada?");
+			double valorHora = num.nextDouble();
+		System.out.println("Quantas horas trabalhadas no mês?");
+			int horasTrab = num.nextInt();
+			
+			double totalMes = valorHora * horasTrab;
+			
+		System.out.printf("Você recebeu R$ %.2f.", totalMes);
+    }
+    }
